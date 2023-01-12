db0ac4314831e8c3044efe6d6796d992c88fcdfa
Nick <nick@Nicks-MacBook-Pro-2.local>
2023-01-12 16:15:23 -0500

    Trying to set up the physics model

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"image/color"
 	"fmt"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -12,14 +13,15 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/data/binding"
 
 ) 
 
 
-func mass_button_group() fyne.CanvasObject { 
+func mass_button_group(str binding.String) fyne.CanvasObject { 
 
 	mass_label := widget.NewLabel("Mass 1:") 
-	mass_data := widget.NewLabel("500kg") 
+	mass_data := widget.NewLabelWithData(str) 
 
 	mass_entry := widget.NewEntry() 
 
@@ -37,12 +39,12 @@ func mass_button_group() fyne.CanvasObject {
 
 }
 
-func generate_side_menu() fyne.CanvasObject { 
+func generate_side_menu(str binding.String) fyne.CanvasObject { 
 
 	// First create buttons 
 
-	group_1 := mass_button_group()
-	group_2 := mass_button_group()
+	group_1 := mass_button_group(str)
+	group_2 := mass_button_group(str)
 
 	restart_sim_button := widget.NewButton("Restart Simulation", func() {fmt.Println("Restart Clicked")})
 	start_button := widget.NewButton("Start!", func() {fmt.Println("Start sim clicked")})
@@ -82,14 +84,23 @@ func main() {
 
 	rect.SetMinSize(fyne.NewSize(1200, 400))
 
-	side_menu := generate_side_menu()
+	str := binding.NewString()
+	go func() {
+		dots := "....."
+		for i := 5; i >= 0; i-- {
+			str.Set("Count down" + dots[:i])
+			time.Sleep(time.Second)
+		}
+		str.Set("Blast off!")
+	}()
+
+	side_menu := generate_side_menu(str)
 
 	content := container.New(layout.NewHBoxLayout(), side_menu, rect) 
 
 	// text4 := canvas.NewText("centered", color.White) 
 
 	// centered:= container.New(layout.NewHBoxLayout(), layout.NewSpacer(), text4, layout.NewSpacer())
-
 
 	main_window.SetContent(content)
 
